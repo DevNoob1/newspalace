a0496f9797c175309aab86fcdc7beadf395f4fd6
Ayush <ayushkr.r1@gmail.com>
2024-06-20 15:21:43 +0530

    first commit

--- src/fetch.js
+++ src/fetch.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const apiUrl = 'https://newsapi.org/v2/top-headlines';
 
 export const fetchArticles = async (category, page, keyword) => {
     const apiKey = process.env.REACT_APP_API_KEY;
+    const apiUrl = process.env.REACT_APP_API_URL;
     const url = `${apiUrl}?apiKey=${apiKey}&page=${page}&pageSize=10${category ? `&category=${category}` : ''}${keyword ? `&q=${keyword}` : ''}`;
     const response = await axios.get(url);
     return response.data;
